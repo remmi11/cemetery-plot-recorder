397bdcf4c7fb7a37411a362b4e7297a25879c29e
Ashik Zinnat Khan <ashrik@gmail.com>
2025-01-01 03:57:07 +0600

    chore: updated staging url configuration

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,8 +1,8 @@
 export const DEV_IPS = {
-	'localhost': 'http://34.233.97.91:8080/dj',
-	// 'localhost': 'http://localhost:8000/dj',
-	'staging': 'https://furmanrecords.com/dj',
+	// 'localhost': 'http://34.233.97.91:8080/dj',
+	'localhost': 'http://localhost:8000/dj',
+	'staging': 'https://higgins.gdi-cemetery-plot-recorder.com/dj',
 };
 // dev
-// export const env = 'staging';
-export const env = 'localhost';
+export const env = 'staging';
+// export const env = 'localhost';
